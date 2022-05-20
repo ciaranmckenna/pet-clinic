2d3dfe302bf767aaea9227f6f8075f336a9f1c3c
Ciaran McKenna <hello@ciaranmckenna.com>
2022-05-20 13:40:27 +0100

    Refactor repeated interface methods. Closes #11.

--- pet-clinic-data/src/main/java/services/VetService.java
+++ pet-clinic-data/src/main/java/services/VetService.java
@@ -2,13 +2,6 @@ package services;
 
 import com.ciaranmckenna.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save (Vet vet);
-
-    Set<Vet> findAll();
 }
